1a5ee93885b334bae08577739d36fe6a58943688
Patrick Michalik <120058021+patrickmichalik@users.noreply.github.com>
2024-12-10 21:39:23 +0100

    Add `Preference.update`, convert Gradle files to Kotlin, and more

--- core/src/main/kotlin/com/patrykandpatrick/opto/core/PreferenceManager.kt
+++ core/src/main/kotlin/com/patrykandpatrick/opto/core/PreferenceManager.kt
@@ -3,18 +3,16 @@ package com.patrykandpatrick.opto.core
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 
-interface PreferenceManager {
-    val preferencesDataStore: DataStore<Preferences>
+public interface PreferenceManager {
+    public val dataStore: DataStore<Preferences>
 
-    fun <S> preference(
-        key: Preferences.Key<S>,
-        defaultValue: S,
-    ) = preference(key = key, defaultValue = defaultValue, serialize = { it }, deserialize = { it })
-
-    fun <C, S> preference(
+    public fun <C, S> preference(
         key: Preferences.Key<S>,
         defaultValue: C,
         serialize: (C) -> S,
         deserialize: (S) -> C,
-    ) = PreferenceImpl(key, defaultValue, serialize, deserialize, preferencesDataStore)
+    ): PreferenceImpl<C, S> = PreferenceImpl(key, defaultValue, serialize, deserialize, dataStore)
+
+    public fun <S> preference(key: Preferences.Key<S>, defaultValue: S): PreferenceImpl<S, S> =
+        preference(key = key, defaultValue = defaultValue, serialize = { it }, deserialize = { it })
 }
